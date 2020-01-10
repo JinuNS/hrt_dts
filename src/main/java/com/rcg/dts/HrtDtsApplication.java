33294775f0cd3f51e410952b0523a9c1943cddbe
Jinu Shaji <jinu.shaji@rcggs.com>
2020-01-10 11:11:22 +0530

    Initial commit

---
+++ src/main/java/com/rcg/dts/HrtDtsApplication.java
@@ -0,0 +1,13 @@
+package com.rcg.dts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrtDtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrtDtsApplication.class, args);
+	}
+
+}
