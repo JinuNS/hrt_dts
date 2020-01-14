1dc7f462f66bc9b935c45beec0a13b1d58922e42
renjithrcggs <Renjith.Kunjiramakurup@rcggs.com>
2020-01-14 15:32:57 +0530

    Dummy Login and changes in application.prop

---
+++ src/main/java/com/rcg/hrtdts/dto/LoginDto.java
@@ -0,0 +1,38 @@
+package com.rcg.hrtdts.dto;
+
+public class LoginDto {
+	
+	private String userName;
+	
+	private String pwd;
+	
+	private String role;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
+
+}
