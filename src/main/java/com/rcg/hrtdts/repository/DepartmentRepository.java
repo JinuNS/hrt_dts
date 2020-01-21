dbbf26bc9ac8cb570b8e9a698465f3bbf4d07955
NeenaJoseph-rcggs <neena.joseph@rcggs.com>
2020-01-21 16:47:37 +0530

    user validations added

--- src/main/java/com/rcg/hrtdts/repository/DepartmentRepository.java
+++ src/main/java/com/rcg/hrtdts/repository/DepartmentRepository.java
@@ -1,10 +1,16 @@
 package com.rcg.hrtdts.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.rcg.hrtdts.model.DepartmentModel;
 
+/**
+ * 
+ * @author neena
+ * @version 1.0
+ * @since 2020-01-14
+ * 
+ **/
 public interface DepartmentRepository extends JpaRepository<DepartmentModel, Long>{
 
 
--- src/main/java/com/rcg/hrtdts/repository/RoleRepository.java
+++ src/main/java/com/rcg/hrtdts/repository/RoleRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rcg.hrtdts.model.RoleModel;
 
+/**
+ * 
+ * @author neena
+ * @version 1.0
+ * @since 2020-01-14
+ * 
+ **/
 public interface RoleRepository  extends JpaRepository<RoleModel, Long>{
 
 }
